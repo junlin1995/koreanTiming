c82eee3a1b73884c50cc6bb3171348550ed7bd77
junlin <aqf77465208@gmail.com>
2019-08-28 14:43:32 +0800

    arranged 5

--- KoreaSnake/src/KoreaFish.java
+++ KoreaSnake/src/KoreaFish.java
@@ -1,7 +1,20 @@
 import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
+
 public class KoreaFish  {
 	
+	private boolean left = false;
+	private boolean right = false;
+	private boolean up = false;
+	private boolean down = false;
+
+	private ImageIcon rightmouth;
+	private ImageIcon upmouth;
+	private ImageIcon downmouth;
+	private ImageIcon leftmouth;
+	private ImageIcon snakeimage;
+	private int lengthofsnake = 3;
 	private int[] snakexlength = new int[750];
 	private int[] snakeylength = new int[750];
 	private int moves = 0;
@@ -16,6 +29,37 @@ public class KoreaFish  {
 			snakeylength[1] = 250;
 			snakeylength[0] = 250;
 		}
+		
+		for (int a = 0; a < lengthofsnake; a++) {
+			if (a == 0) { 
+				if (right) {
+					rightmouth = new ImageIcon("rightmouth.png"); 
+					rightmouth.paintIcon(this, g, snakexlength[a], snakeylength[a]);
+				} 
+				else if (left) {
+					leftmouth = new ImageIcon("leftmouth.png"); 
+					leftmouth.paintIcon(this, g, snakexlength[a], snakeylength[a]);
+				}
+				else if (up) {
+					upmouth = new ImageIcon("upmouth.png"); 
+					upmouth.paintIcon(this, g, snakexlength[a], snakeylength[a]);
+				} 
+				else if (down) {
+					downmouth = new ImageIcon("downmouth.png"); 
+					downmouth.paintIcon(this, g, snakexlength[a], snakeylength[a]);
+				} 
+				else {
+					rightmouth = new ImageIcon("rightmouth.png"); 
+					rightmouth.paintIcon(this, g, snakexlength[a], snakeylength[a]);
+				}
+			}
+
+			if (a != 0) {
+				snakeimage = new ImageIcon("fishbody.png"); 
+				snakeimage.paintIcon(this, g, snakexlength[a], snakeylength[a]);
+			}
+		}
+
 	}
 
 }
